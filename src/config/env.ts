fe15cb4057474589b2d9b56cdb3f61410e7dbc24
Rick <ricleyreis@gmail.com>
2025-10-02 18:02:48 -0300

    chroe: ajustando variaveis de ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,8 @@
 // Configurações de ambiente para o Supabase
 export const ENV = {
   SUPABASE: {
-    URL: "https://hrhfvjnfhhaabuhozknu.supabase.co",
-    ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhyaGZ2am5maGhhYWJ1aG96a251Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkxNjEyNjEsImV4cCI6MjA2NDczNzI2MX0.PuQkHnwTVTG8BfMjRMCTA0KGbHPdlNS84joUwVkJ-BY"
+    URL: import.meta.env.VITE_SUPABASE_URL,
+    ANON_KEY: import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY,
   }
 } as const;
 
